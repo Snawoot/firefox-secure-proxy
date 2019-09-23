54b78125c98199ae95a16d9dfc0c649e0b15a282
Vladislav Yarmak <vladislav@vm-0.com>
2019-09-23 15:30:44 +0300

    WIP

---
+++ firefox_secure_proxy/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+USER_AGENT = "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"
+CLIENT_ID = "a8c528140153d1c6"
+FXA_PROVIDER_URL = "https://accounts.firefox.com"
+FXA_PROFILE_SCOPE = "profile"
+FXA_PROXY_SCOPE = "https://identity.mozilla.com/apps/secure-proxy"
+FXA_REDIRECT_URL = "https://cb7cbf5bedba243279adcd23bc6b88de7a304388.extensions.allizom.org/"
--- firefox_secure_proxy/login.py
+++ firefox_secure_proxy/login.py
@@ -10,45 +10,49 @@ from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 from oic import rndstr
 from oic.oic.message import AuthorizationResponse, RegistrationResponse
 
-USER_AGENT = 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
+from . import constants
 
-CLIENT_ID = "a8c528140153d1c6"
-
-def main():
+def get_token(*, user_agent=constants.USER_AGENT):
     client = Client(client_authn_method=None)
-    client.store_registration_info(RegistrationResponse(client_id=CLIENT_ID))
-    provider_info = client.provider_config('https://accounts.firefox.com')
+    client.store_registration_info(RegistrationResponse(client_id=constants.CLIENT_ID))
+    provider_info = client.provider_config(constants.FXA_PROVIDER_URL)
     ch_args, cv = client.add_code_challenge()
-    session = {}
-    session["state"] = rndstr()
-    session["nonce"] = rndstr()
+    session = {
+        "state": rndstr(),
+        "nonce": rndstr(),
+    }
     args = {
         "access_type": "offline",
-        "client_id": CLIENT_ID,
+        "client_id": constants.CLIENT_ID,
         "response_type": "code",
-        "scope": ["profile", "https://identity.mozilla.com/apps/secure-proxy"],
+        "scope": [constants.FXA_PROFILE_SCOPE, constants.FXA_PROXY_SCOPE],
         "state": session["state"],
         "nonce": session["nonce"],
-        "redirect_uri": "https://cb7cbf5bedba243279adcd23bc6b88de7a304388.extensions.allizom.org/",
+        "redirect_uri": constants.FXA_REDIRECT_URL,
     }
     args.update(ch_args)
 
     auth_req = client.construct_AuthorizationRequest(request_args=args)
     login_url = auth_req.request(client.authorization_endpoint)
     print("Please follow this URL, authenticate and paste back URL where you was redirected.")
+    print("It's OK if URL leads to dead page, just copy&paste it's URL here.")
     print("")
     print(login_url)
     rp_url = input("Please paste redirect URL:").strip()
     aresp = client.parse_response(AuthorizationResponse, info=rp_url, sformat="urlencoded")
     args = {
         "code": aresp["code"],
-        "client_id": CLIENT_ID,
+        "client_id": constants.CLIENT_ID,
         "code_verifier": cv,
     }
     resp = client.do_access_token_request(state=aresp["state"],
                                           request_args=args,
-                                          authn_method=None)
-    print(resp)
+                                          authn_method=None,
+                                          http_args={"headers": { "User-Agent": user_agent}})
+    return resp
+
+def main():
+    print(get_token())
 
 if __name__ == '__main__':
     main()
